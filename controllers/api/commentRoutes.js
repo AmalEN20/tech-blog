de9a9380ba4740aa69faa7cf201ecc47578b665e
AmalStudy <amalengstudy@gmail.com>
2023-05-03 01:03:28 -0700

    update models/

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -74,4 +74,3 @@ router.delete('/:id', withAuth, async (req, res) => {
 });
 
 module.exports = router;
-
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,63 +1,63 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { User } = require('../../models');
 
-// Create a new post
-router.post('/', withAuth, async (req, res) => {
+// Create a new user
+router.post('/', async (req, res) => {
     try {
-        const newPost = await Post.create({
-            ...req.body,
-            user_id: req.session.user_id,
-        });
+        const userData = await User.create(req.body);
 
-        res.status(200).json(newPost);
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
-// Update a post
-router.put('/:id', withAuth, async (req, res) => {
+// Login
+router.post('/login', async (req, res) => {
     try {
-        const postData = await Post.update({
-            title: req.body.title,
-            content: req.body.contents
-        }, {
-            where: {
-                id: req.params.id,
-                user_id: req.session.user_id,
-            },
-        });
+        const userData = await User.findOne({ where: { username: req.body.username } });
 
-        if (!postData) {
-            res.status(404).json({ message: 'No post found with this id!' });
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password, please try again' });
             return;
         }
 
-        res.status(200).json(postData);
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+
     } catch (err) {
-        res.status(500).json(err);
+        res.status(400).json(err);
     }
 });
 
-// Delete a post
-router.delete('/:id', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.destroy({
-            where: {
-                id: req.params.id,
-                user_id: req.session.user_id,
-            },
+// Logout
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
         });
-
-        if (!postData) {
-            res.status(404).json({ message: 'No post found with this id!' });
-            return;
-        }
-
-        res.status(200).json(postData);
-    } catch (err) {
-        res.status(500).json(err);
+    } else {
+        res.status(404).end();
     }
 });
 
